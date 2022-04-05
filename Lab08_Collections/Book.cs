e74b66f3d283d6f7234795d2280a5f528e5f264a
fuad-bassam <74737369+fuad-bassam@users.noreply.github.com>
2022-04-06 01:00:32 +0300

    solve lab 8 (Fuad and yousef)

---
+++ Lab08_Collections/Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab08_BookLibrary
+{
+
+
+
+   public class Book
+    {
+        
+        public string Title { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int NumberOfPages { get; set; }
+     
+
+       public Book(string title , string firstName, string lastName, int numberOfPages) {
+
+            this.Title = title;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.NumberOfPages = numberOfPages;
+        
+        }
+
+      
+    }
+}
---
+++ Lab08_Collections/Library.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab08_BookLibrary
+{
+
+   
+    public class Library : ILibrary
+    {
+       
+
+
+        private Dictionary<string, Book> libraryBooks = new Dictionary<string,Book>();
+
+        public int Count { get; set; }
+        public void Add(string title, string firstName, string lastName, int numberOfPages)
+        {
+
+            libraryBooks.Add(title, new Book(title, firstName, lastName, numberOfPages));
+            Console.WriteLine("add book" + title);
+            Count++;
+        }
+
+        public Book Borrow(string title)
+        {
+
+           
+
+            if (libraryBooks.TryGetValue(title,out Book book))
+            {
+               
+                libraryBooks.Remove(title);
+                Console.WriteLine(" Borrow book " + title);
+                Count--;
+                return book;
+            }
+            else
+            {
+                Console.WriteLine("book was not found" );
+                return null;
+            }
+        }
+        public void Return(Book book)
+        {
+
+
+            libraryBooks.Add(book.Title, book);
+            Count++;
+        }
+
+
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+
+            //  var item1= libraryBooks.GetEnumerator();
+
+            //foreach (KeyValuePair<string, Book> item in libraryBooks)
+            //{
+            //    yield return item.Value;
+            //}
+
+
+            foreach ( Book item in libraryBooks.Values)
+            {
+                yield return item;
+            }
+
+
+        }
+
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            foreach (KeyValuePair<string, Book> item in libraryBooks)
+            {
+                yield return item;
+            }
+        }
+    }
+}
